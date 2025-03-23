69cdc7ff9ff2eae38c9f849eb50479ddec12e0b1
Vitor Diniz <dinizadroit@gmail.com>
2025-03-23 22:57:24 +0100

    test app

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,8 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <h1>Hello World!</h1>
   </StrictMode>,
 )
